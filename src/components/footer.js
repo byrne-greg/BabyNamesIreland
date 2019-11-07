b68d50175ef0dc087abd54c11021514c61255901
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-07 15:59:05 +0000

    update of the footer from default gatsby

---
+++ src/components/footer.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+const Footer = () => (
+    <footer style={{marginTop: "2rem", marginBottom: "1rem"}}>
+    {`© ${new Date().getFullYear()}, Built with ❤️ by `} <a href="https://www.linkedin.com/in/byrne-greg/">Greg Byrne</a>
+  </footer>
+)
+
+export default Footer;
