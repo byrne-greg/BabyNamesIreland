4f0354d7385fa20894d60b2f81624a377712b62c
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-08 14:30:42 +0000

    centering person info page title

--- src/components/person-info/page-title.js
+++ src/components/person-info/page-title.js
@@ -2,7 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const PersonInfoPageTitle = ({ title }) => (
-  <h1>{title}</h1>
+  <div style={{ display: `flex`, justifyContent: `center` }}>
+    <h1>{title}</h1>
+  </div>
 )
 
 export default PersonInfoPageTitle
