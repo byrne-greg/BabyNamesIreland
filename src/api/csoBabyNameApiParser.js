a7420ce97168637682c255260c5e6899442c2a69
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-08 12:56:09 +0000

    lint fix to csoBabyNameApiParser

--- src/api/csoBabyNameApiParser.js
+++ src/api/csoBabyNameApiParser.js
@@ -1,16 +1,14 @@
-const axios =require( "axios");
-const JSONstat = require( "jsonstat");
+const axios = require(`axios`)
+const JSONstat = require(`jsonstat`)
 
-module.exports = function getNameData(csoUrl, gender) {
-
+module.exports = function getNameData (csoUrl, gender) {
   console.log(`API_REQUEST: Requesting info from ${csoUrl}`)
   return axios.get(csoUrl).then(response => {
+    const dataset = JSONstat(response.data).Dataset(0)
 
-    const dataset = JSONstat(response.data).Dataset(0);
-
-    const nameDimension = dataset.Dimension("Name");
-    const yearDimension = dataset.Dimension("Year");
-    const statDimension = dataset.Dimension("Statistic");
+    const nameDimension = dataset.Dimension(`Name`)
+    const yearDimension = dataset.Dimension(`Year`)
+    const statDimension = dataset.Dimension(`Statistic`)
 
     const nameData = nameDimension.id.map(nameCode => ({
       id: nameCode,
@@ -18,7 +16,7 @@ module.exports = function getNameData(csoUrl, gender) {
       gender,
       data: yearDimension.id.map(yearCode => {
         const isRankStat = statCode =>
-          statDimension.Category(statCode).label.indexOf("Rank") !== -1;
+          statDimension.Category(statCode).label.indexOf(`Rank`) !== -1
 
         const getStatValue = isRankingStat => {
           const totalNumData = () =>
@@ -27,19 +25,19 @@ module.exports = function getNameData(csoUrl, gender) {
               Year: yearCode,
               Statistic: statDimension.id.filter(
                 statCode =>
-                  isRankingStat ? isRankStat(statCode) : !isRankStat(statCode)
-              )[0]
-            }).value;
-          return totalNumData() !== null ? totalNumData() : 0;
-        };
+                  isRankingStat ? isRankStat(statCode) : !isRankStat(statCode),
+              )[0],
+            }).value
+          return totalNumData() !== null ? totalNumData() : 0
+        }
 
         return {
           year: yearCode,
           total: getStatValue(false),
-          rank: getStatValue(true)
-        };
-      })
-    }));
-    return nameData;
-  });
+          rank: getStatValue(true),
+        }
+      }),
+    }))
+    return nameData
+  })
 }
