b1c8fd122284c813bd64c28bf1a1b4d2aaf310bb
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-27 09:16:54 +0000

    add util to get flattened people data with year

--- src/utils.js
+++ src/utils.js
@@ -13,3 +13,12 @@ export function getRankingSuffix (num) {
   if (num % 10 === 3 && num !== 13) { return `rd` }
   return `th`
 }
+
+export function getFlattenedPeopleWithYearData (nameArray, yearString) {
+  const peopleWithRelevantData = nameArray.filter(person => person.data.filter(yearData => yearData.year === yearString).length > 0)
+  const flattenedPeopleWithRelevantData = peopleWithRelevantData.map(person => { const dataOfYearReq = person.data.filter(yearData => yearData.year === yearString)[0]; return { name: person.name, gender: person.gender, year: dataOfYearReq.year, rank: dataOfYearReq.rank, key: dataOfYearReq.rank, bornCount: dataOfYearReq.total } })
+
+  return flattenedPeopleWithRelevantData
+
+
+}
