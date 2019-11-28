e40fc95dac4c126affc18565c672676e49b71b69
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-28 14:04:41 +0000

    add web icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#589738`,
         theme_color: `#589738`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/bni-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
