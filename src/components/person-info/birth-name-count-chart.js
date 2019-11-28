c0e2a77b76922faf1a7aef4007fbae4d55ec00fc
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-11-28 15:46:53 +0000

    adding curve and average line to birth name count chart

--- src/components/person-info/birth-name-count-chart.js
+++ src/components/person-info/birth-name-count-chart.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { FlexibleWidthXYPlot, LineMarkSeries, LabelSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines } from 'react-vis'
+import { FlexibleWidthXYPlot, LineSeries, LineMarkSeries, LabelSeries, XAxis, YAxis, VerticalGridLines, HorizontalGridLines, Hint } from 'react-vis'
 
 const BirthNameCountChart = ({ birthNameCountData }) => {
-  const birthNameCountChartData = birthNameCountData.map(dataObj => { return { x: dataObj.year, y: dataObj.total } }).sort((a, b) => Number(a.x) - Number(b.x))
+  const birthNameTotalCountLineData = birthNameCountData.map(dataObj => { return { x: dataObj.year, y: dataObj.total } }).sort((a, b) => Number(a.x) - Number(b.x))
 
   // due to the unknown size of the numbers on the x-axis (e.g. 1 -> 1000's), we need to indent the margin when numbers are sufficiently large enough that they clip in the edge
-  const birthNameCountLargestNumber = birthNameCountChartData.reduce((acc, currentValue) => Math.max(acc, currentValue.y), 0)
+  const birthNameCountLargestNumber = birthNameTotalCountLineData.reduce((acc, currentValue) => Math.max(acc, currentValue.y), 0)
   let chartLeftMargin = 40 // default
   if (birthNameCountLargestNumber >= 1000) {
     chartLeftMargin = 45
   }
+
+  const birthNameCountAverage = birthNameTotalCountLineData.reduce((acc, currentValue) => { console.log(acc, currentValue); return acc + currentValue.y }, 0) / birthNameTotalCountLineData.length
+  const birthNameCountAverageLineData = birthNameCountData.map(dataObj => { return { x: dataObj.year, y: birthNameCountAverage } }).sort((a, b) => Number(a.x) - Number(b.x))
 
   return (
     <FlexibleWidthXYPlot xType="ordinal" height={500} margin={{ left: chartLeftMargin }} >
@@ -18,8 +21,9 @@ const BirthNameCountChart = ({ birthNameCountData }) => {
       <HorizontalGridLines />
       <XAxis title="year" tickPadding={5} tickSizeInner={500} tickLabelAngle={-90} />
       <YAxis title="births registered"/>
-      <LineMarkSeries data={birthNameCountChartData} />
-      <LabelSeries data={birthNameCountChartData} getLabel={d => `${d.y}`} style={{ fontSize: `0.5rem` }} />
+      <LineMarkSeries data={birthNameTotalCountLineData} curve={`curveMonotoneX`} />
+      <LineSeries data={birthNameCountAverageLineData} strokeStyle={`dashed`}/>
+      <LabelSeries data={birthNameTotalCountLineData} getLabel={d => `${d.y}`} style={{ fontSize: `0.5rem` }} />
     </FlexibleWidthXYPlot>
   )
 }
