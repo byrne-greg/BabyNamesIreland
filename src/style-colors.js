1431bbec09a92d6e018bf443e1fe7b29fbd2e345
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-12-02 09:57:39 +0000

    fixing primary green color

--- src/style-colors.js
+++ src/style-colors.js
@@ -1,4 +1,7 @@
+const primaryGreen = `#589738`
+
 export default {
+  PRIMARY_GREEN: primaryGreen,
   CARD: {
     FEMALE_BACKGROUND_COLOR: `#ffd6e7`,
     MALE_BACKGROUND_COLOR: `#bae7ff`,
@@ -9,7 +12,7 @@ export default {
     DECREASE: `#cf1322`,
   },
   HEADER: {
-    BACKGROUND: `#589738`,
+    BACKGROUND: primaryGreen,
   },
 
 }
