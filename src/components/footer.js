a7cbc2c468700cc098868bfc4bbb472641a63d26
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-12-02 09:50:06 +0000

    send footer link to about page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { LinkedInLink } from "./links"
+import { navigate } from 'gatsby'
+import routes from '../routes'
 
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer style={{ marginTop: `2rem`, marginBottom: `1rem` }}>
-      {`© ${currentYear === 2019 ? `2019` : `2019-${currentYear}`}, Built with ❤️ by `} <LinkedInLink/>
+      {`© ${currentYear === 2019 ? `2019` : `2019-${currentYear}`}, Built with ❤️ by `} <span style={{ color: `#1890ff`, cursor: `pointer` }}onClick={() => navigate(routes.ABOUT)}>Greg Byrne</span>
     </footer>
   )
 }
