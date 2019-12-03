d27815d2faf28abbfe12f71c7b0366c9de5d3d85
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-12-03 10:03:15 +0000

    add menu navigation and fluid logo text

--- src/components/header.js
+++ src/components/header.js
@@ -1,56 +1,83 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
-import { LinkButton } from "./button"
+import PropTypes from "prop-types"
+import { Link, navigate, withPrefix } from "gatsby"
+import { Menu } from 'antd'
 import styleColors from "../style-colors"
 import routes from "../routes"
 import BniTextLogo from "./image"
 
-const Header = ({ headerGap = true, siteTitle }) => (
-  <header
-    style={{
-      background: styleColors.HEADER.BACKGROUND,
-      marginBottom: headerGap ? `1.45rem` : 0,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        display: `flex`,
-        justifyContent: `space-between`,
-        flexDirection: `column`,
-      }}
-    >
-      {/* <h1 style={{ margin: 0 }}> */}
-      <div id="bni-text-logo" style={{ width: 300 }}>
-        <Link
-          to="/"
+const Header = ({ headerGap = true }) => {
+  return (
+    <>
+      <header
+        style={{
+          background: styleColors.HEADER.BACKGROUND,
+        }}
+      >
+        <div
           style={{
-            color: `white`,
-            textDecoration: `none`,
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `1.45rem 1.0875rem`,
+            display: `flex`,
+            justifyContent: `space-between`,
+            flexDirection: `column`,
           }}
         >
-          <BniTextLogo />
-        </Link>
-      </div>
-      {/* </h1> */}
-      <div >
-        <LinkButton type="default" to={routes.ABOUT} style={{ backgroundColor: `white`, margin: `0.5rem 1rem` }}>About</LinkButton>
-        <LinkButton type="default" to={routes.NAME_SEARCH} style={{ backgroundColor: `white`, margin: `0.5rem 1rem` }}>Search</LinkButton>
-      </div>
-    </div>
-  </header>
-)
+          <section style={{ maxWidth: 350 }}>
+            <Link
+              to="/"
+              style={{
+                color: `white`,
+                textDecoration: `none`,
+              }}
+            >
+              <BniTextLogo />
+            </Link>
+          </section>
+        </div>
+      </header>
+      <NavigationMenu headerGap={headerGap}/>
+    </>
+  )
+}
 
 Header.propTypes = {
-  siteTitle: PropTypes.string,
   headerGap: PropTypes.bool,
 }
 
-Header.defaultProps = {
-  siteTitle: ``,
+export default Header
+
+const NavigationMenu = ({ headerGap }) => {
+  const keyRouteMap = [{ key: `1`, route: routes.HOME }, { key: `2`, route: routes.NAME_SEARCH }, { key: `3`, route: routes.ABOUT }]
+
+  const setSelectedMenuKey = () => {
+    const keyRoute = keyRouteMap.filter((keyRoute) => location.pathname === withPrefix(keyRoute.route))[0]
+    return keyRoute ? keyRoute.key : `0`
+  }
+
+  return (
+    <section>
+      <Menu
+        mode="horizontal"
+        selectedKeys={[setSelectedMenuKey()]}
+        style={{ lineHeight: `3rem`, display: `flex`, justifyContent: `center`, marginBottom: headerGap ? `1.45rem` : 0 }}
+        onClick={(e) => {
+          keyRouteMap.forEach(keyRoute => {
+            if (keyRoute.key === e.key) {
+              navigate(keyRoute.route)
+            }
+          })
+        }}
+      >
+        <Menu.Item key="1">Home</Menu.Item>
+        <Menu.Item key="2" >Search</Menu.Item>
+        <Menu.Item key="3">About</Menu.Item>
+      </Menu>
+    </section>
+  )
 }
 
-export default Header
+NavigationMenu.propTypes = {
+  headerGap: PropTypes.bool,
+}
