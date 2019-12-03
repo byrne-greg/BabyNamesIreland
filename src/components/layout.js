36dec3537a8edcf29a3c985067a7d21a24bc740f
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2019-12-03 09:10:33 +0000

    comment out site title and page padding fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,31 +7,32 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Footer from "./footer"
 import "./layout.css"
+// import { useStaticQuery, graphql } from "gatsby"
 
 const Layout = ({ hero = null, children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
-      <Header headerGap={hero === null} siteTitle={data.site.siteMetadata.title} />
+      <Header headerGap={hero === null} />
       {hero}
       <div
         style={{
           margin: `0 auto`,
           maxWidth: 1025,
-          padding: 0,
+          padding: `0px 1.0875rem 1.45rem`,
+          paddingTop: 0,
         }}
       >
         <main>{children}</main>
