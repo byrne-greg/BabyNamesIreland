8f599bd86a8bd56239120fe87a03c760a0c8cafd
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-03-03 17:38:31 +0000

    refactor of the cso api and parser

--- server/src/csoAPI.js
+++ server/src/csoAPI.js
@@ -1,4 +1,4 @@
-const getNameData = require(`./csoRegisteredBirthJsonStatParser.js`)
+const getNameData = require(`./csoBabyNameJsonStatParser.js`)
 
 /**
  * Constructs a single data object from the CSO Births Registered In Ireland for Boys and Girls (VSA10 & VSA11)
@@ -28,26 +28,28 @@ async function getBirthNameDataFromCSO () {
 
 /**
  * Constructs a single data object from the CSO Names In Ireland with 3 or More Occurrences for Boys and Girls (VSA50 & VSA60)
+ * 
+ * An example of the data object can be found in the server/test folder
  */
-async function getOccurenceNameDataFromCSO () {
+async function getNameOccurrenceNameDataFromCSO () {
   // fetch raw data from the birth name CSO statbank api
-  const fetchBirthRegistrationBoysNames = () => getNameData(
+  const fetchOccurrencesBoysNames = () => getNameData(
     `https://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/VSA50`,
     `Male`,
   )
 
-  const fetchBirthRegistrationGirlsNames = () => getNameData(
-    `https://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/VSA61`,
+  const fetchOccurrencesGirlsNames = () => getNameData(
+    `https://www.cso.ie/StatbankServices/StatbankServices.svc/jsonservice/responseinstance/VSA60`,
     `Female`,
   )
    // await for results
-   const res1 = await fetchBirthRegistrationBoysNames()
-   const res2 = await fetchBirthRegistrationGirlsNames()
-   const birthNames = [...res1.nameData, ...res2.nameData];
+   const res1 = await fetchOccurrencesBoysNames()
+   const res2 = await fetchOccurrencesGirlsNames()
+   const occurrences = [...res1.nameData, ...res2.nameData];
     // we are removing any person data that has zero for rank and total. this indicates the CSO didn't record or the data was noted as statistically unreliable
-    birthNames.forEach(person => { person.data = person.data.filter(dataObj => dataObj.rank !== 0 && dataObj.total !== 0) })
+    occurrences.forEach(person => { person.data = person.data.filter(dataObj => dataObj.rank !== 0 && dataObj.total !== 0) })
 
-   return { data: birthNames, lastRecordedYear: res1.lastRecordedYear }
+   return { data: occurrences, lastRecordedYear: res1.lastRecordedYear }
   }
 
-  module.exports = { getBirthNameDataFromCSO, getOccurenceNameDataFromCSO }
+  module.exports = { getBirthNameDataFromCSO, getNameOccurrenceNameDataFromCSO }
--- server/test/csoAPI.test.js
+++ server/test/csoAPI.test.js
@@ -1,18 +1,17 @@
-const { default: Item } = require("antd/lib/list/Item")
-
 const csoAPI = require("../src/csoAPI")
-const testResponse = require("./csoRegisteredBirthJsonResult.example.json")
 
 describe("CSO API tests", () => {
   describe("Births Registered in Ireland", () => {
     test("should match test response object (2020)", async () => {
+      const testResponse = require("./csoRegisteredBirthJsonResult.example.json")
       const response = await csoAPI.getBirthNameDataFromCSO()
       expect(response).toMatchObject(testResponse)
     })
   })
-  describe("Occurences of Name in Ireland", () => {
-    xtest("should match test response object (2021)", async () => {
-      const response = await csoAPI.getOccurenceNameDataFromCSO()
+  describe("Occurrences of Name in Ireland", () => {
+    test("should match test response object (2021)", async () => {
+      const testResponse = require("./csoNameOccurrenceJsonResult.example.json")
+      const response = await csoAPI.getNameOccurrenceNameDataFromCSO()
       expect(response).toMatchObject(testResponse)
     })
   })
