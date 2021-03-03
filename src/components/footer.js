0396fec6fc7c456deee0f995bf8a4a445d12e04c
byrne-greg <5464272+byrne-greg@users.noreply.github.com>
2021-03-03 15:38:06 +0000

    change heart style on footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,8 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer style={{ marginTop: `2rem`, marginBottom: `1rem` }}>
-      {`© ${currentYear === 2019 ? `2019` : `2019-${currentYear}`}, Built with ❤️ by `} <span style={{ color: `#1890ff`, cursor: `pointer` }}onClick={() => navigate(routes.ABOUT)}>Greg Byrne</span>
+      {`© ${currentYear === 2019 ? `2019` : `2019-${currentYear}`}, Built with ♡ by `} 
+      <span style={{ color: `#1890ff`, cursor: `pointer` }}onClick={() => navigate(routes.ABOUT)}>Greg Byrne</span>
     </footer>
   )
 }
